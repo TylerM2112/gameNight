4c667d7badcb6882610f1ef8e3b2ec14a8c86c55
Tyler <millert2420@gmail.com>
2018-09-02 11:40:54 -0700

    added general input component

--- src/screens/LandingPage/LandingPage.js
+++ src/screens/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './LandingPage.css';
 
+import DefaultInput from '../../components/utils/DefaultInput/DefaultInput';
+
 class LandingPage extends Component {
   render() {
     return (
@@ -10,6 +12,7 @@ class LandingPage extends Component {
           <img src={logo} className="LandingPage-logo" alt="logo" />
           <h1 className="LandingPage-title">Welcome to Game Night!</h1>
         </header>
+        <DefaultInput placeholder="add your game here!">game title</DefaultInput>
       </div>
     );
   }
