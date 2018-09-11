ed49a24066e58ca013d56e5b5b5192d0aa50defe
Brandon Mase <brandonofsid@gmail.com>
2018-09-10 22:38:56 -0700

    started working on terraforming mars component

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import React, { Fragment }from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './screens/LandingPage/LandingPage.js';
+import TerraformingMars from './components/games/TerraformingMars/TerraformingMars'
 
 export default <Fragment>
     <Route exact path="/" component={LandingPage} />
+    <Route path="/terraformingMars" component={TerraformingMars} />
    </Fragment>
